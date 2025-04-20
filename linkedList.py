fe800265f00384c8c11e09f7d016f914ca5996de
Erjan Carlo Poblete <132138873+crazy47@users.noreply.github.com>
2025-04-20 15:16:25 +0800

    Update linkedList.py

--- linkedList.py
+++ linkedList.py
@@ -8,8 +8,6 @@ LinkedList :
 _____________________________________________________________________________________________________
 """
 import re
-
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -17,8 +15,6 @@ class Node:
 class linkedList:
     def __init__(self):
         self.head = None
-
-
     def updateIteration(self):
         nodes = []
         current = self.head
@@ -29,9 +25,6 @@ class linkedList:
             nodes.append(str(current.data))
             current = current.next
         print(" -> ".join(nodes))
-
-
-
     def sortHelper(self, num): 
         newnoDE = Node(num)
         if self.head is None:
@@ -46,16 +39,11 @@ class linkedList:
             current = current.next
         newnoDE.next = current.next
         current.next = newnoDE
-
-
     def deleter(self, num): 
         self.updateIteration()
         while self.head and self.head.data == num:
             self.head = self.head.next
         current = self.head
-       
-
-
         while current and current.next: 
             if current.next.data == num:
                 current.next = current.next.next 
@@ -70,21 +58,15 @@ class linkedList:
         for i in arr:
             self.deleter(i)
             self.updateIteration()
-            
 session = True
-
 while session:
-
     userInput = str(input("Enter a non-sequentail, comma-delumited, list of 10 integerts: "))
     allNum = re.findall(r'-?\d+', userInput) 
     new_allNum = [int(i) for i in allNum]
     test = linkedList()
     test.addSort(new_allNum)
-
     toDelete = str(input("Enter a comma-delimited, lsit of integers for removeal in the list: "))
     allocatedNum = re.findall(r'-?\d+', toDelete)
     convertedNum = [int(i) for i in allocatedNum]
-
     test.deleteAlike(convertedNum)
-
     session = False
